0236b9b37f3713e8bc2e7fd0e280a2e4c14a2456
Sarthak Verma <sarthakvdev@gmail.com>
2024-12-23 04:30:12 +0530

    init eido

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        Chat with Eido
+      </main>
+    </div>
+  );
+}
